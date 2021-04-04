2ba57d0a7aee9b4be2b5509f62b3aea15bbad645
andreealarisa624 <62350385+git829@users.noreply.github.com>
2021-04-04 13:07:55 +0300

    Setup refacut

--- src/main/java/org/example/NitriteExemple.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class NitriteExemple {
-
-    Nitrite db = Nitrite.builder()
-            .compressed()
-            .filePath("/tmp/test.db")
-            .openOrCreate("user", "password");
-}
