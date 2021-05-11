0abb56df4aca16c38304147bc5a6980b5e9bfea3
andreealarisa624 <62350385+git829@users.noreply.github.com>
2021-05-11 21:15:44 +0300

    AA-28

---
+++ src/main/java/org/example/exceptions/ProductDoesNotExist.java
@@ -0,0 +1,17 @@
+package org.example.exceptions;
+
+public class ProductDoesNotExist extends Exception{
+    private String name;
+
+    public ProductDoesNotExist(String name) {
+        super(String.format("An product with the name %s does not exists!", name));
+        this.name=name;
+    }
+
+    public String getUsername() {
+        return name;
+    }
+}
+
+
+
